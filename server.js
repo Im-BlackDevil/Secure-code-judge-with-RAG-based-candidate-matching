bf922fc6aee49f16585ce5a9e688260481340bd7
Subham Agarwal <agarwalsubham18072004@gmail.com>
2026-09-04 20:35:42 +0530

    Resume upload storing raw text

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(express.json());
 app.use('/auth', require('./src/routes/auth'));
 app.use('/jobs', require('./src/routes/jobs'));
 app.use('/submissions', require('./src/routes/submissions'));
+app.use('/resumes', require('./src/routes/resumes'));
 
 app.get('/health', (req, res) => res.json({ status: 'ok' }));
 
